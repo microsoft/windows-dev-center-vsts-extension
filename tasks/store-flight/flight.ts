82a29d831716d2e5c8df619a640f1562d6dd09cd
Frederic Lafrance <t-frlafr@microsoft.com>
2016-08-18 17:44:44 -0700

    Added definitions/stubs for flight task

---
+++ tasks/store-flight/flight.ts
@@ -0,0 +1,12 @@
+/*
+ * Behavior for the Publish task. Takes authentication information, app information, and packages,
+ * and flights to the Store.
+ */
+
+
+// Currently only stubs
+
+export function flightTask()
+{
+    console.log('Hello flighting');
+}
---
+++ tasks/store-flight/flightUi.ts
@@ -0,0 +1,13 @@
+/**
+ * Entry point for the Flight task. Gathers parameters and performs validation.
+ */
+
+// Currently stubs
+
+import fli = require('./flight');
+
+import tl = require('vsts-task-lib');
+
+
+tl.debug('Flighting task');
+fli.flightTask();
