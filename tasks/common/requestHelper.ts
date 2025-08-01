a1175759a142dbab80021051989c11d147b62476
Louis Wu <tianyanwu95@gmail.com>
2025-08-01 15:27:48 -0700

    address cg bugs

--- tasks/common/requestHelper.ts
+++ tasks/common/requestHelper.ts
@@ -1,14 +1,7 @@
-﻿/*
- * A helper to perform various HTTP requests, with some default handling to manage errors.
- * This is mainly a wrapper for the 'request' npm module that uses promises instead of callbacks.
- */
-
-import http = require('http'); // Only used for types
-
-import {v4 as uuidv4} from 'uuid';
+﻿import { v4 as uuidv4 } from 'uuid';
 import Q = require('q');
-import request = require('request');
 import tl = require('azure-pipelines-task-lib');
+import axios, { AxiosError, AxiosRequestConfig, AxiosResponse } from 'axios';
 
 /** How long to wait between retries (in ms) */
 const RETRY_DELAY = 60000;
@@ -20,76 +13,53 @@ const TIMEOUT = 600000;
 const UPLOAD_BLOCK_SIZE_BYTES = 1024 * 1024; // 1Mb
 
 /** Credentials used to gain access to a particular resource. */
-export interface Credentials
-{
-    /** The tenant ID associated with these credentials. */
+export interface Credentials {
     tenant: string;
-
     clientId: string;
     clientSecret: string;
 }
 
 /** A token used to access a particular resource. */
-export interface AccessToken
-{
-    /** Resource to which this token grants access. */
-    resource: string
-
-    /** Credentials used to obtain access. */
+export interface AccessToken {
+    resource: string;
     credentials: Credentials;
-
-    /** Expiration timestamp of the token */
     expiration: number;
-
-    /** Actual token to be used in the request. */
     token: string;
 }
 
 /** Whether an access token should be renewed. */
-function isExpired(token: AccessToken): boolean
-{
-    // Date.now() returns a number in miliseconds.
-    // We say that a token is expired if its expiration date is at most five seconds in the future.
+function isExpired(token: AccessToken): boolean {
     return (Date.now() / 1000) + 5 > token.expiration;
 }
 
 /** All the information given to us by the request module along a response. */
-export class ResponseInformation
-{
+export class ResponseInformation {
     error: any;
-    response: http.IncomingMessage;
+    response: AxiosResponse | undefined;
     body: any;
 
-    constructor(_err: any, _res: http.IncomingMessage, _bod: any)
-    {
+    constructor(_err: any, _res: AxiosResponse | undefined, _bod: any) {
         this.error = _err;
         this.response = _res;
         this.body = _bod;
     }
 
-    // For friendly logging
-    toString(): string
-    {
-        var log: string;
+    toString(): string {
+        let log: string;
 
-        if (this.error != undefined)
-        {
+        if (this.error != undefined) {
             log = `Error ${JSON.stringify(this.error)}`;
-        }
-        else
-        {
-            var bodyToPrint = this.body;
-            if (typeof bodyToPrint != 'string')
-            {
+        } else {
+            let bodyToPrint = this.body;
+            if (typeof bodyToPrint != 'string') {
                 bodyToPrint = JSON.stringify(bodyToPrint);
             }
-            var statusCode: string = (this.response != undefined && this.response.statusCode != undefined) ? this.response.statusCode.toString() : 'unknown';
+            let statusCode: string = (this.response != undefined && this.response.status != undefined) ? this.response.status.toString() : 'unknown';
             log = `Status ${statusCode}: ${bodyToPrint}`;
         }
 
         if (this.response != undefined &&
-            this.response.headers['ms-correlationid'] != undefined)
-        {
+            this.response.headers['ms-correlationid'] != undefined) {
             log = log + ` CorrelationId: ${this.response.headers['ms-correlationid']}`;
         }
 
@@ -98,123 +68,67 @@ export class ResponseInformation
 }
 
 /**
- * Perform a request with some default handling.
+ * Perform a request with some default handling using axios.
  *
- * For convenience, parses the body if the content-type is 'application/json'.
- * Further, examines the body and logs any errors or warnings.
- *
- * If an transport or application level error occurs, rejects the returned promise.
- * The reason given is an instance of @ResponseInformation@, containing the error
- * object, the response and the body.
- *
- * If no error occurs, resolves the returned promise with the body.
- *
- * @param options Options describing the request to execute.
- * @param stream If specified, pipe this stream into the request.
+ * @param options AxiosRequestConfig describing the request to execute.
  */
-export function performRequest<T>(
-    options: (request.UriOptions | request.UrlOptions) & request.CoreOptions,
-    stream?: NodeJS.ReadableStream):
-    Q.Promise<T>
-{
-    var deferred = Q.defer<T>();
+export function performRequest<T>(options: AxiosRequestConfig): Q.Promise<T> {
+    const deferred = Q.defer<T>();
 
-    if (options.timeout == undefined)
-    {
+    if (options.timeout === undefined) {
         options.timeout = TIMEOUT;
     }
 
     // Log correlation Id for better diagnosis
-    var correlationId = uuidv4();
+    const correlationId = uuidv4();
     tl.debug(`Starting request with correlation id: ${correlationId}`);
-    if (options.headers === undefined)
-    {
-        options.headers = {
-            'CorrelationId': correlationId
-        }
+    if (!options.headers) {
+        options.headers = {};
     }
-    else
-    {
-        options.headers['CorrelationId'] = correlationId;
-    }
+    options.headers['CorrelationId'] = correlationId;
 
-    var callback = function (error, response, body)
-    {
-        // For convenience, parse the body if it's JSON.
-        if (response != undefined && // response is undefined if a transport-level error occurs
-            response.headers['content-type'] != undefined &&   // content-type is undefined if there is no content
-            response.headers['content-type'].indexOf('application/json') != -1 &&
-            typeof body == 'string') // body might be an object if the options given to request already parsed it for us
-        {
-            body = JSON.parse(body);
-            logErrorsAndWarnings(response, body);
-        }
-
-        if (error || (response && response.statusCode != undefined && response.statusCode >= 400))
-        {
+    axios(options)
+        .then((response: AxiosResponse<T>) => {
+            logErrorsAndWarnings(response, response.data);
+            deferred.resolve(response.data);
+            tl.debug(`Finished request with correlation id: ${correlationId}`);
+        })
+        .catch((error: AxiosError) => {
+            let response = error.response;
+            let body = response ? response.data : undefined;
             deferred.reject(new ResponseInformation(error, response, body));
-        }
-        else
-        {
-            deferred.resolve(body);
-        }
-        tl.debug(`Finished request with correlation id: ${correlationId}`);
-    }
-
-    if (!stream)
-    {
-        request(options, callback);
-    }
-    else
-    {
-        stream.pipe(request(options, callback));
-    }
+            tl.debug(`Finished request with correlation id: ${correlationId}`);
+        });
 
     return deferred.promise;
 }
 
 /**
- * Same as @performRequest@, but additionally requires an authentification token.
+ * Same as @performRequest@, but additionally requires an authentication token.
  * @param auth A token used to identify with the resource. If expired, it will be renewed before executing the request.
  */
 export function performAuthenticatedRequest<T>(
     auth: AccessToken,
-    options: (request.UriOptions | request.UrlOptions) & request.CoreOptions):
-    Q.Promise<T>
-{
+    options: AxiosRequestConfig
+): Q.Promise<T> {
     // The expiration check is a function that returns a promise
-    var expirationCheck = function ()
-    {
-        if (isExpired(auth))
-        {
-            return authenticate(auth.resource, auth.credentials).then(function (newAuth)
-            {
+    const expirationCheck = function () {
+        if (isExpired(auth)) {
+            return authenticate(auth.resource, auth.credentials).then(function (newAuth) {
                 auth.token = newAuth.token;
                 auth.expiration = newAuth.expiration;
             });
-        }
-        else
-        {
-            /* This looks strange, but it returns a promise for void, which is exactly what we need. */
+        } else {
             return Q.when();
         }
     };
 
-
-    return expirationCheck() // Call the expiration check to obtain a promise for it.
-        .then<T>(function () // Chain the use of the token to that promise.
-        {
-            if (options.headers === undefined)
-            {
-                options.headers = {
-                    'Authorization': 'Bearer ' + auth.token
-                }
+    return expirationCheck()
+        .then<T>(function () {
+            if (!options.headers) {
+                options.headers = {};
             }
-            else
-            {
-                options.headers['Authorization'] = 'Bearer ' + auth.token;
-            }
-
+            options.headers['Authorization'] = 'Bearer ' + auth.token;
             return performRequest<T>(options);
         });
 }
@@ -224,42 +138,36 @@ export function performAuthenticatedRequest<T>(
  * @param credentials Credentials to use for authentication.
  * @returns Promises an access token to use to communicate with the resource.
  */
-export function authenticate(resource: string, credentials: Credentials): Q.Promise<AccessToken>
-{
-    var endpoint = 'https://login.microsoftonline.com/' + credentials.tenant + '/oauth2/token';
-    var requestParams = {
+export function authenticate(resource: string, credentials: Credentials): Q.Promise<AccessToken> {
+    const endpoint = 'https://login.microsoftonline.com/' + credentials.tenant + '/oauth2/token';
+    const requestParams = new URLSearchParams({
         grant_type: 'client_credentials',
         client_id: credentials.clientId,
         client_secret: credentials.clientSecret,
         resource: resource
-    };
+    });
 
-    var options = {
+    const options: AxiosRequestConfig = {
         url: endpoint,
         method: 'POST',
-        form: requestParams
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+        data: requestParams.toString()
     };
 
     console.log('Authenticating with server...');
-    return performRequest<any>(options).then<AccessToken>(body =>
-    {
-        var tok: AccessToken = {
+    return performRequest<any>(options).then<AccessToken>(body => {
+        const tok: AccessToken = {
             resource: resource,
             credentials: credentials,
             expiration: body.expires_on,
             token: body.access_token
         };
-
         return tok;
     });
 }
 
 /**
  * Transforms a promise so that it is tried again a specific number of times if it fails.
- *
- * A 'generator' of promises must be supplied. The reason is that if a promise fails,
- * then it will stay in a failed state and it won't be possible to await on it anymore.
- * Therefore a new promise must be returned every time.
  *
  * @param numRetries How many times should the promise be tried to be fulfilled.
  * @param promiseGenerator A function that will generate the promise to try to fulfill.
@@ -268,43 +176,31 @@ export function authenticate(resource: string, credentials: Credentials): Q.Prom
 export function withRetry<T>(
     numRetries: number,
     promiseGenerator: () => Q.Promise<T>,
-    errPredicate?: ((err: any) => boolean)): Q.Promise<T>
-{
-    return promiseGenerator().fail(err =>
-    {
-        if (numRetries > 0 && (!errPredicate || errPredicate(err)))
-        {
-            var randomDelay: number = Math.floor(Math.random() * RETRY_DELAY + RETRY_DELAY); // RETRY_DELAY <= randomDelay  < 2 * RETRY_DELAY
+    errPredicate?: ((err: any) => boolean)
+): Q.Promise<T> {
+    return promiseGenerator().fail(err => {
+        if (numRetries > 0 && (!errPredicate || errPredicate(err))) {
+            const randomDelay: number = Math.floor(Math.random() * RETRY_DELAY + RETRY_DELAY);
             console.log(`Operation failed with ${err}`);
             console.log(`Waiting ${randomDelay / 1000} seconds then retrying... (${numRetries - 1} retrie(s) left)`);
             return Q.delay(randomDelay).then(() => withRetry(numRetries - 1, promiseGenerator, errPredicate));
-        }
-        else
-        {
-            /* Don't wrap err in an error because it's already an error
-            (.fail() is the equivalent of "catch" for promises) */
+        } else {
             throw err;
         }
     });
 }
 
-/** 
- * Indicates whether the given object is an HTTP response for a retryable error. 
+/**
+ * Indicates whether the given object is an HTTP response for a retryable error.
  * @param err The error returned by the API
  * @param relax Whether the function will return true for most error codes or not
- * @description The Windows Store returns 429 and 503 for retryable errors. Relaxing the check will return true also for any error code greater or equal to 500
  */
-export function isRetryableError(err:any, relax:boolean = true): boolean
-{
-    // Does this look like a ResponseInformation?
-    if (err != undefined && err.response != undefined && typeof err.response.statusCode == 'number')
-    {
-        return err.response.statusCode == 429 // 429 code is returned by the API for throttle down. This is retriable
-            || err.response.statusCode == 503
-            || (relax && err.response.statusCode >= 500)
+export function isRetryableError(err: any, relax: boolean = true): boolean {
+    if (err != undefined && err.response != undefined && typeof err.response.status == 'number') {
+        return err.response.status == 429
+            || err.response.status == 503
+            || (relax && err.response.status >= 500);
     }
-
-    // Default to retry if no err information.
     return true;
 }
 
@@ -312,29 +208,23 @@ export function isRetryableError(err:any, relax:boolean = true): boolean
  * Examines a response body and logs errors and warnings.
  * @param response Response returned by the Store API
  * @param body A body in the format given by the Store API
- * (Where body.statusDetails.errors and body.statusDetails.warnings
- * are arrays of objects containing 'code' and 'details' attributes).
  */
-function logErrorsAndWarnings(response: any, body: any)
-{
+function logErrorsAndWarnings(response: AxiosResponse<any>, body: any) {
     if (body === undefined || body.statusDetails === undefined)
         return;
 
-    if (Array.isArray(body.statusDetails.errors) && body.statusDetails.errors.length > 0)
-    {
+    if (Array.isArray(body.statusDetails.errors) && body.statusDetails.errors.length > 0) {
         console.error('Errors occurred in request');
         (<any[]>body.statusDetails.errors).forEach(x => console.error(`\t[${x.code}]  ${x.details}`));
     }
 
-    if (Array.isArray(body.statusDetails.warnings) && body.statusDetails.warnings.length > 0)
-    {
+    if (Array.isArray(body.statusDetails.warnings) && body.statusDetails.warnings.length > 0) {
         tl.debug('Warnings occurred in request');
         (<any[]>body.statusDetails.warnings).forEach(x => tl.debug(`\t[${x.code}]  ${x.details}`));
     }
 
     if (response != undefined &&
-        response.headers['ms-correlationid'] != undefined)
-    {
+        response.headers['ms-correlationid'] != undefined) {
         tl.debug(`CorrelationId: ${response.headers['ms-correlationid']}`);
     }
 }
